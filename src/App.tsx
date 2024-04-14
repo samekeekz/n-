022895d37e402534f62471d7dcd49d2965501030
samatbelentbay <belentbais03@mail.ru>
2024-04-14 20:20:56 +0500

    init project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Route, Routes } from 'react-router-dom'
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  )
+}
+
+export default App
